2c7b6ec20aa574a0e2972273f8f431ff9b4b0997
Your Name <you@example.com>
2025-11-16 15:07:15 +0100

    fichier prompt isolé

--- getNewsFront_macro/src/pages/NewsRssPage.jsx
+++ getNewsFront_macro/src/pages/NewsRssPage.jsx
@@ -6,6 +6,7 @@ import NewsJSONItem from "../components/NewsJSONItem";
 import "./NewsPages.css";
 import "../App.css";
 import loadingGif from "../assets/pictures/Loading.gif";
+import prompts from "../assets/prompts";
 
 const NewsRssPage = (props) => {
 	/**
@@ -97,19 +98,6 @@ const NewsRssPage = (props) => {
 	 * 📌 Data à envoyer, doit respecter un certain format pour être autorisé
 	 */
 	const [inputText, setInputText] = useState(``);
-
-	const promptSelectNewsSociety = {
-		prompt: `Je fais des posts sur des sujets entre actualité et sujet de sociétés sur la Corée 
-		J'ai mes sources avec, tous les matins, une centaine d'articles potentiellement interessant 
-		Voici certains sélectionnés sur les 15 derniers jours 2025-10-25 19:41:36 6 unlicensed tour guides caught leading foreign tourists in Seoul https://www.koreatimes.co.kr/lifestyle/travel-food/20251024/6-unlicensed-tour-guides-caught-leading-foreign-tourists-in-seoul Voir le contenu 2025-10-24 08:54:02 How criminal networks in Cambodia ensnare Koreans https://koreajoongangdaily.joins.com/news/2025-10-23/national/politics/How-criminal-networks-in-Cambodia-ensnare-Koreans/2426343 Voir le contenu 2025-10-23 13:07:48 Dear Korea, can we please stop with Konglish names? https://www.koreaherald.com/article/10559351 Voir le contenu 2025-10-22 18:02:41 Mom protects daughter from being hit by e-scooter, sustains severe injuries https://www.koreaherald.com/article/10598900 Voir le contenu 2025-10-21 06:56:03 In Korea's ring-heavy dating culture, is wedding band still essential? - The Korea Herald https://www.koreaherald.com/article/10594143 Voir le contenu 2025-10-20 20:56:06 As off-limit Seoraksan peak draws influencers, park service urges removal of social media posts https://www.koreaherald.com/article/10597063 Voir le contenu 2025-10-20 20:54:59 7 in 10 South Korean law students from high-income families: report https://www.koreaherald.com/article/10591179 Voir le contenu 2025-10-20 20:52:07 Public support for unification dips below 50% for 1st time: poll https://www.koreaherald.com/article/10597217 Voir le contenu 2025-10-20 20:48:33 Korea’s 'gyeran-ppang' named one of world’s 50 best breads by CNN https://www.koreatimes.co.kr/lifestyle/travel-food/20251020/koreas-gyeran-ppang-named-one-of-worlds-50-best-breads-by-cnn Voir le contenu 2025-10-20 13:52:11 CNN classe le « gyeran-ppang » parmi les 50 meilleurs pains du monde https://world.kbs.co.kr/service/news_view.htm?lang=f&Seq_Code=92432&fbclid=IwY2xjawNjAZZleHRuA2FlbQIxMABicmlkETA1UWVwTHZaVW5NNHNVRnUzAR6TfnB-JIxvEMYM_nG6YMBBKuzj2jkhJLEeX9a-YSzilVBw8UsVc7QHQur4YA_aem_GyRSL3OKsix_1SdPCvv2oA Voir le contenu 2025-10-20 13:28:36 Foreign exchange student's lost tuition money safely returned thanks to a good samaritan https://www.allkpop.com/buzz/2025/08/foreign-exchange-students-lost-tuition-money-safely-returned-thanks-to-a-good-samaritan 
-		En regardant la liste suivante , en vois tu qui pourrait être interessant et coller à mon axe ?`,
-		redirectUrl: "https://chatgpt.com/c/69158ace-749c-832c-8682-d6935ed4c39d",
-	};
-
-	const promptSelectNewsDaily = {
-		prompt: `Voici une liste de news. Je cherche à voir quels sont les sujets récurrents OU les sujets qui pourraient intéresser un public international. L'essentiel est que ces sujets marquent l'actualité (= actualité majeure en Corée) OU intéressent les non-coréens (= parmi les sujets qui intéressent les non-coréens : la kpop, les dramas, la culture, le tourisme, les faits divers...). Note que les news abordant un sujet international sans que la Corée soit directement ou indirectement concernée ne m’intéressent pas.\n\nJ'aimerais que tu me sélectionnes les titres de ces news-là et que tu me les mettes dans un tableau (au sens JS du terme).\n\nExemple : [{\"keyword\": \"test\", \"title\": \"titre de l'article…\"}, {\"keyword\": \"test\", \"title\": \"titre de l'article…\"}] avec en keyword un mot-clef relativement précis (sans espace) qui représente le sujet de l'article.\n\nIl faudra garder le titre exact (c’est très important, il ne faut aucun retraitement, même si tu vois des choses étranges comme &apos; ⇒ tu dois garder le titre exact) pour que je puisse retrouver l’entrée dans ma BDD. Format JSON pour envoyer via requête.\n\nDonc : {\"data\": […]}\n\nAinsi si deux news abordent exactement le même sujet (ne pas être trop généraliste sur le choix du keyword, il faut vraiment que les articles soient sur un sujet très proche), elles devront avoir le même keyword. Néanmoins, tu peux remonter des articles uniques (sans autre article partageant le même sujet dans la liste).\n\nOrdonne les par keyword.`,
-		redirectUrl: "https://claude.ai/new",
-	};
 
 	// Get news from RSS feeds
 	const handleFetchRssNews = useCallback(async () => {
@@ -420,31 +408,29 @@ const NewsRssPage = (props) => {
 	return (
 		<div className="general-bloc">
 			<h2>Get news from RSS feeds and get a JSON</h2>
-			{currentView === "none" && (
-				<div>
-					<p>Veuillez sélectionner une option pour afficher les nouvelles.</p>
-				</div>
-			)}
+			{currentView === "none" && <p>Que souhaitez vous faire ?</p>}
 			<div className="view_bloc">
 				<button className="view_tag" onClick={() => switchView("rss")}>
-					RSS view
+					Liste des news récupérées lors du dernier appel
 				</button>
 				<button
 					className="view_tag"
 					onClick={() => switchView("rss_simplified")}
 				>
-					RSS view simplified
+					Vue simplifiée des news récupérées lors du dernier appel
 				</button>
 				<button className="view_tag" onClick={() => switchView("json")}>
-					Given News
+					Liste des news validées pour sauvegarde
 				</button>
+			</div>
+			<div className="view_bloc">
 				<button
 					className="ActionBtn"
 					onClick={() => {
 						handleFetchRssNews();
 					}}
 				>
-					Get RSS news to select
+					Obtenir des news
 				</button>
 				<button
 					className="ActionBtn"
@@ -452,16 +438,15 @@ const NewsRssPage = (props) => {
 						handleFetchPendingJsonNews();
 					}}
 				>
-					News w/ description
+					Voir les news sélectionnées et description
 				</button>
-
 				<button
 					className="ActionBtn"
 					onClick={() => {
 						switchView("form_to_send_news");
 					}}
 				>
-					(AI) Send chosen RSS news
+					Soumettre liste de news
 				</button>
 			</div>
 			{isLoading && (
@@ -502,10 +487,9 @@ const NewsRssPage = (props) => {
 							className="actionBtn"
 							onClick={() => {
 								const textToCopy = `${
-									promptSelectNewsSociety.prompt
+									prompts.promptSelectNewsSociety.prompt
 								}\n\n${rssNewsList.map((el) => el.title).join("\n")}`;
-								navigator.clipboard.writeText(textToCopy);
-								window.open(promptSelectNewsSociety.redirectUrl, "_blank");
+								window.open(prompts.promptSelectNewsSociety.redirectUrl, "_blank");
 							}}
 						>
 							Faire une sélection d'articles de société
@@ -514,10 +498,12 @@ const NewsRssPage = (props) => {
 							className="actionBtn"
 							onClick={() => {
 								const textToCopy = `${
-									promptSelectNewsDaily.prompt
+									prompts.promptSelectNewsDaily.prompt
 								}\n\n${rssNewsList.map((el) => el.title).join("\n")}`;
-								navigator.clipboard.writeText(textToCopy);
-								window.open(promptSelectNewsDaily.redirectUrl, "_blank");
+								window.open(
+									prompts.promptSelectNewsDaily.redirectUrl,
+									"_blank"
+								);
 							}}
 						>
 							Faire une sélection d'articles d'actualité
